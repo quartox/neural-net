f8c603b1ce8df8d7a992c398dd57e6b41ab83839
jlord <Jesse.Lord@allstate.com>
2017-02-03 12:05:55 -0600

    Working backprop with matrix of weights per layer

--- network.py
+++ network.py
@@ -1,19 +1,24 @@
 import numpy as np
 
 class Network:
-    def __init__(self, num_inputs, num_neurons):
+    def __init__(self, num_predictors, num_neurons):
         """Creates the layers of neurons in a neural network.
 
         Args:
-          num_inputs - the number of inputs for the first layer of the network.
+          num_predictors - the number of predictors
           num_neurons - a list of the number of neurons in the hidden and output
               layers of the network.
         """
-        self.layers = [Layer(num_neurons[0], num_inputs)]
-        for layer_index in range(1, len(num_neurons)):
-            current_layer_num_inputs = self.layers[-1].num_neurons + 1
-            self.layers.append(
-                Layer(current_layer_num_inputs, num_neurons[layer_index]))
+        self.num_predictors = num_predictors
+        self.num_layers = len(num_neurons)
+        self.layers = [Layer(self.num_predictors + 1, num_neurons[0])]
+        for i_layer in range(1, self.num_layers):
+            num_inputs = len(self.layers[-1].outputs)
+            if i_layer < self.num_layers-1:
+                self.layers.append(Layer(num_inputs, num_neurons[i_layer]))
+            else:
+                self.layers.append(Layer(num_inputs, num_neurons[i_layer],
+                                         is_output=True))
 
     def feedforward(self, x):
         """Given the input record x, computes the output of the neural network.
@@ -23,21 +28,38 @@ class Network:
           x - an input record (list-like).
 
         Returns:
-          the output of the network (a float or numpy.array of floats).
+          A list of numpy arrays of outputs
         """
-        current_layer_inputs = x
+        outputs = []
+        inputs = np.ones(self.num_predictors+1)
+        inputs[1:] = x
         for layer in self.layers:
-            current_layer_outputs = layer.compute_outputs(current_layer_inputs)
-            current_layer_inputs = np.insert(current_layer_outputs, 0, 1)
-        if len(current_layer_outputs) == 1:
-            return current_layer_outputs[0]
-        else:
-            return current_layer_outputs
+            outputs.append(layer.compute_outputs(inputs))
+            inputs = outputs[-1]
+        return outputs
+
+    def backprop(self, x, y):
+        outputs = self.feedforward(x)
+        deltas = []
+        deltas.append((2 * (self.layers[-1].outputs - y) *
+                       self.activation_derivative(self.layers[-1].outputs)))
+        for i_layer in range(self.num_layers-2, -1, -1):
+            num_neurons = self.layers[i_layer].num_neurons
+            weighted_deltas = np.zeros(self.layers[i_layer].num_neurons)
+            for i_neuron in range(num_neurons):
+                weighted_deltas[i_neuron] = np.dot(
+                    self.layers[i_layer+1].weights[i_neuron+1,:], deltas[-1])
+            activation_deriv = self.activation_derivative(outputs[i_layer][1:])
+            deltas.append(np.multiply(activation_deriv, weighted_deltas))
+        return deltas
+
+    @staticmethod
+    def activation_derivative(x):
+        return 1 - np.square(x)
 
 class Layer:
-    def __init__(self, num_inputs, num_neurons):
-        """Creates a layer of neurons in a neural network. Sets the seed
-        to ensure that no two neurons are initialized with the same weights/bias.
+    def __init__(self, num_inputs, num_neurons, is_output=False):
+        """Creates a layer of neurons in a neural network.
 
         Args:
           num_inputs - the number of inputs to each neuron in this layer.
@@ -45,10 +67,13 @@ class Layer:
         """
         self.num_neurons = num_neurons
         self.num_inputs = num_inputs
-        self.neurons = []
-        for i in range(self.num_neurons):
-            np.random.seed(i)
-            self.neurons.append(Neuron(self.num_inputs))
+        self.weights = np.random.randn(self.num_inputs, self.num_neurons)
+        self.is_output = is_output
+        if self.is_output:
+            self.outputs = np.empty(num_neurons)
+        else:
+            self.outputs = np.ones(num_neurons+1)
+        self.deltas = np.empty(num_neurons)
 
     def compute_outputs(self, inputs):
         """Computes the output of each neuron in this layer.
@@ -59,21 +84,8 @@ class Layer:
         Returns:
           the outputs of the neurons in this layer, a numpy.array
         """
-        outputs = []
-        for neuron in self.neurons:
-            outputs.append(neuron.compute_output(inputs))
-        return np.array(outputs)
-
-class Neuron:
-    def __init__(self, num_inputs):
-        """Creates a neuron."""
-        self.num_inputs = num_inputs
-        self.weights = np.random.randn(self.num_inputs)
-        self.bias = np.random.randn()
-
-    def compute_output(self, inputs):
-        return self.sigmoid(np.inner(self.weights, inputs) + self.bias)
-
-    @staticmethod
-    def sigmoid(z):
-        return 1.0/(1.0 + np.exp(-z))
+        if self.is_output:
+            self.outputs = np.tanh(np.dot(inputs, self.weights))
+        else:
+            self.outputs[1:] = np.tanh(np.dot(inputs, self.weights))
+        return self.outputs
